1e104fd818b1827124894dde48882bfd4d5311d8
ThinkDifferentGit <brandonftwas@gmail.com>
2025-03-13 15:23:23 -0700

    Add files via upload

---
+++ BrandonMarroquinStopLight.py
@@ -0,0 +1,12 @@
+def stop_light(_stop_light):
+print(_stop_light)
+
+
+def main():
+print("Welcome To The STEAM Clown Makey Bot")
+
+main()
+
+Dictionary:
+
+stop_light = {'red_LED' : 1, 'yellow_LED' : 1, 'green_LED' : 1}
